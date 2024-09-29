755cd46e946d4792ddb253662acaf8228e7f654b
Phil Samuel <philipmayr@outlook.com>
2024-09-29 10:03:54 -0500

    Update algorithm.rb

--- algorithm.rb
+++ algorithm.rb
@@ -1,7 +1,7 @@
 # Prime Modulus Quadradic Residuosity Decider Algorithm
 
 def decide_quadradic_residuosity(p, x)
-    b = x ** (((p - 1) / 2) % p)
+    b = (x ** ((p - 1) / 2)) % p
     
     if b == 1
         return true 
@@ -10,18 +10,22 @@ def decide_quadradic_residuosity(p, x)
     end
 end
 
-# prime p
-print "Enter a prime number: "
-p = gets.to_i
-
-# quadradic residue candidate
-print "Enter a quadradic residue candidate modulo " + p.to_s + ": "
-x = gets.to_i
-
-puts
-
-if decide_quadradic_residuosity(p, x)
-    puts x.to_s + " is a quadradic residue modulo " + p.to_s + "."
-else
-    puts x.to_s + " is a quadradic nonresidue modulo " + p.to_s + "."
+loop do
+    # get prime p
+    print "Enter a prime number: "
+    p = gets.to_i
+    
+    # get quadradic residue candidate
+    print "Enter a quadradic residue candidate modulo " + p.to_s + ": "
+    x = gets.to_i
+    
+    puts
+    
+    if decide_quadradic_residuosity(p, x)
+        puts x.to_s + " is a quadradic residue modulo " + p.to_s + "."
+    else
+        puts x.to_s + " is a quadradic nonresidue modulo " + p.to_s + "."
+    end
+    
+    puts
 end
